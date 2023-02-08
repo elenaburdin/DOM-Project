a7eed7c5de68df64907d267ed6fff4cf53f76f7f
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-02-08 19:38:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+
+// Form submit event
+form.addEventListener('submit', addItem);
+
+// Delete event
+itemList.addEventListener('click', removeItem);
+
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// add item
+function addItem(e) {
+    e.preventDefault();
+
+    // Get Input Value
+    var newItem = document.getElementById('item').value;
+
+    // Create new il element
+    var li = document.createElement('li');
+    // add class
+    li.className = 'list-group-item';
+    // add text note with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    // Create delete button element
+    var deleteBtn = document.createElement('button');
+
+    // add classes to delete button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    // append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // append button to li
+    li.appendChild(deleteBtn);
+
+    // append li to list
+    itemList.appendChild(li);
+
+}
+
+// Remove Items
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')) {
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+// Filter Items
+function filterItems(e) {
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to array
+    Array.from(items).forEach(function (item) {
+        var itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
